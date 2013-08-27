9083f89a0b45ceab12750f996cd191405f17d7aa
Joseph Boiteau <josephboiteau@gmail.com>
2013-08-27 10:37:53 +1000

    updated the git::client package
    thx shr3kst3r -- https://github.com/shr3kst3r/puppetlabs-git/blob/abfdcfc81a6bf379de918fdd2cd03892a63302b4/manifests/init.pp

--- manifests/client.pp
+++ manifests/client.pp
@@ -1,5 +1,21 @@
+# Class: git
+#
+# This class installs git client
+#
+# Sample Usage:
+#  include git::client
+#
+
 class git::client {
-  package { "git-core":
-    ensure => present,
+  case $operatingsystem {
+    centos, redhat, debian, ubuntu: { $package_name = 'git' }
+    default: { $package_name = 'git-core' }
+  }
+
+  package { $package_name:
+    ensure => installed,
+  }
+}
+
   }
 }
